f1087fde54c2fbf6124a8a7df97fe80f047fc360
Sean Song <songjinshan@gmail.com>
2019-11-28 20:48:12 +1300

    Update bithumb.js

--- js/bithumb.js
+++ js/bithumb.js
@@ -893,7 +893,7 @@ module.exports = class bithumb extends Exchange {
             'address': address,
             'currency': currency['id'],
         };
-        if (['XRP', 'XMR', 'XLM', 'XEM', 'EOS', 'STEEM', 'GXC', 'IOST'].includes(currency['id'])) {
+        if (['XRP', 'XMR', 'XLM', 'XEM', 'EOS', 'STEEM', 'GXC', 'IOST', 'WAXP'].includes(currency['id'])) {
             const destination = this.safeString (params, 'destination');
             if ((tag === undefined) && (destination === undefined)) {
                 throw new ArgumentsRequired (this.id + ' ' + code + ' withdraw() requires a tag argument or an extra destination param');
