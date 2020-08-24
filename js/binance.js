1432ee5af580281f88aab878bdf66a1a03ee73c8
Sean Song <songjinshan@gmail.com>
2020-08-25 00:55:59 +1200

    Update binance.js

--- js/binance.js
+++ js/binance.js
@@ -542,6 +542,7 @@ module.exports = class binance extends Exchange {
             };
             const status = this.safeString (market, 'status');
             const active = (status === 'TRADING');
+            if (!active) continue; // do not include unlisted pairs in markets
             const margin = this.safeValue (market, 'isMarginTradingAllowed', future);
             const entry = {
                 'id': id,
